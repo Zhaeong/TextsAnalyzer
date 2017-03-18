42a2d877e6c0d5f97b0b73a270a1a1ed8a7bfd0d
Zhaeong <owen_zzz@hotmail.com>
2017-03-17 22:21:48 -0400

    added text getting funtion

--- app/src/main/java/com/example/owen/textsanalyzer/MainActivity.java
+++ app/src/main/java/com/example/owen/textsanalyzer/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button button = (Button) findViewById(R.id.getMessages);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getMessage();
+            }
+        });
+
+
+
+    }
+
+    protected void getMessage()
+    {
         List<String> sms = new ArrayList<String>();
 
         if (ContextCompat.checkSelfPermission(MainActivity.this,
